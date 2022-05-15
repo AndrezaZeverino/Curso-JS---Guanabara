13d9f4b10305992cb705f20db3c3ee4c4c1de81b
Andreza Zeverino <andrezazeverino@gmail.com>
2022-05-15 16:00:08 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 70;
+
+console.log(`A velocidade do seu carro é: ${vel}.`);
+
+if (vel >= 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!');
+} else {
+    console.log('Dirija sempre usando cinto de segurança');
+}
